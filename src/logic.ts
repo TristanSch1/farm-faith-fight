3a3f1fe91ea342cee3c223c556fb5c53f3645eb7
Tristan Schwartz <tristan@lesentrecodeurs.com>
2023-07-23 15:38:05 +0200

    chore: prettier / eslint config

--- src/logic.ts
+++ src/logic.ts
@@ -1,38 +1,38 @@
-import type { RuneClient } from "rune-games-sdk/multiplayer"
+import type { RuneClient } from "rune-games-sdk/multiplayer";
 
 export interface GameState {
-  count: number
+  count: number;
 }
 
 type GameActions = {
-  increment: (params: { amount: number }) => void
-}
+  increment: (params: { amount: number }) => void;
+};
 
 declare global {
-  const Rune: RuneClient<GameState, GameActions>
+  const Rune: RuneClient<GameState, GameActions>;
 }
 
 export function getCount(game: GameState) {
-  return game.count
+  return game.count;
 }
 
 Rune.initLogic({
   minPlayers: 2,
   maxPlayers: 4,
   setup: (): GameState => {
-    return { count: 0 }
+    return { count: 0 };
   },
   actions: {
     increment: ({ amount }, { game }) => {
-      game.count += amount
+      game.count += amount;
     },
   },
   events: {
     playerJoined: () => {
-      // Handle player joined
+      console.log("Player joined");
     },
     playerLeft() {
       // Handle player left
     },
   },
-})
+});
