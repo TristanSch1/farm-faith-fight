50dce0720b32d397dbb290fba2f75c4d0cf2c9bc
Kevin UNFRICHT <unfricht.kevin@hotmail.fr>
2023-07-27 19:52:34 +0200

    WIP (UI) swipe animation of card

--- ui/src/components/Card/CardDrawable.tsx
+++ ui/src/components/Card/CardDrawable.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useMotionValue, useTransform, useAnimation } from 'framer-motion';
+import { motion, useMotionValue, useTransform, useAnimation } from 'framer-motion';
 
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
@@ -9,10 +9,30 @@ type Props = {
     children: React.ReactElement,
 }
 
-const CardDrawable: React.FC<Props> = ({ children }) => (
-    <div className={styles.card_draw_pile}>
-        {children}
-    </div>
-)
+const CardDrawable: React.FC<Props> = ({ children }) => {
+    const dragMotionValue = useMotionValue(0);
+    const cardTransformSwipeValue = useTransform(dragMotionValue, [-200, 200], [-50, 50]);
+    const cardOpacityValue = useTransform(dragMotionValue, [-200, -10, 0, 10, 200], [0, .8, 1, .8, 0]);
+    const animationController = useAnimation();
+
+    return (
+        <motion.div
+            style={{
+                height: '100%',
+                zIndex: 1000,
+                position: 'relative',
+                transform: cardTransformSwipeValue,
+                opacity: cardOpacityValue
+            }}
+            drag="x"
+            whileDrag={{
+                scale: 1.1
+            }}
+            dragConstraints={{ left: -100, right: 100 }}
+        >
+            {children}
+        </motion.div>
+    );
+}
 
 export default CardDrawable;
